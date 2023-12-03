ca0d21c92ea22ca197889c3c0b12aafeeafcd873
FKQ7 <fayooz050050@gmail.com>
2023-12-03 18:10:31 +0300

    stage2.5 without fornt-end

--- SB/SB/urls.py
+++ SB/SB/urls.py
@@ -30,6 +30,27 @@ urlpatterns = [
     path('register/',user_views.register, name="register"),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),    
     path('logout/',auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"), 
+   path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
 ]
 
 if settings.DEBUG:#if this doesn't work in deployment check https://docs.djangoproject.com/en/2.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
--- SB/users/models.py
+++ SB/users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image as Img
+from django.core.files import File
+from django.db import models
 # Create your models here.
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
